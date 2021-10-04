84e6ebc536d6510f6ae4dabe3343decf87df8d19
Andrew Hong <akyle2001@gmail.com>
2021-10-04 12:24:48 -0400

    Button colour override

--- codebase/frontend/src/components/SubmitButton.tsx
+++ codebase/frontend/src/components/SubmitButton.tsx
@@ -32,20 +32,21 @@ const SubmitButton = ({
     <PillButton
       className={`${className} ${
         disabled ? "button-disabled" : "cursor-pointer"
-      } transition-all ease-in-out duration-500 disabled:opacity-50 py-2 h-10 w-10 object-scale-down px-6 rounded-md bg-blue-600`}
+      } transition-all ease-in-out duration-500 ${state === "success" ? "bg-green-400" : "bg-blue-600 disabled:opacity-50"} py-2 h-10 w-10 object-scale-down px-6 rounded-md `}
       disabled={disabled}
       type="submit"
     >
       {state === "success" ? (
         <div className="flex flex-row text-center">
-          <div className="mr-1">{successText}</div>
+          <div className="mr-3">{successText}</div>
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="w-6"
+            className="w-5"
             fill="currentColor"
             viewBox="0 0 16 16"
           >
-            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+            <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
           </svg>
         </div>
       ) : state === "pending" ? (
@@ -57,7 +58,7 @@ const SubmitButton = ({
           />
         </div>
       ) : (
-        <div>{ defaultText }</div>
+        <div>{defaultText}</div>
       )}
     </PillButton>
   );
