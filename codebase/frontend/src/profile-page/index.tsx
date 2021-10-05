a747582dc78f6c4da1dbf3315e782cca49f0c821
dz365 <dwqz365@gmail.com>
2021-10-04 23:11:13 -0400

    Completion of skills section on profile page

--- codebase/frontend/src/profile-page/index.tsx
+++ codebase/frontend/src/profile-page/index.tsx
@@ -13,10 +13,61 @@ import {
 
 import ModalVideo from "react-modal-video";
 import "../../node_modules/react-modal-video/scss/modal-video.scss";
+import Section from "./section";
+import CloseButton from "react-bootstrap/CloseButton";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import { createInputFiles } from "typescript";
 const ProfilePage = () => {
   const [isOpen, setOpen] = useState(false);
+  const [skillslist, setList] = React.useState(["Java", "Python", "C"]);
 
+  const add_skill = (event) => {
+    event.preventDefault();
+    const new_skill = event.target.skill.value
+    const new_list = skillslist.concat(new_skill)
+    setList(new_list)
+    event.target.reset()
+  };
+  const delete_skill = (remove_skill) => {
+    const new_list = skillslist.filter((skill) => skill !== remove_skill);
+    setList(new_list);
+  };
+
+  const summary = (
+    <p className="mx-4">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolorum iure
+      blanditiis officia vero ipsa magnam ipsum sit quidem veritatis, quam
+      quaerat laboriosam corporis harum recusandae odio error reprehenderit
+      autem?
+    </p>
+  );
+
+  const displayskills = (
+    <div className="flex flex-row w-full flex-wrap">
+      {skillslist.map((skill) => (
+        <div className="flex items-center bg-gray-300 mx-4 mb-4 p-2" key={skill}>
+          <CloseButton onClick={() => delete_skill(skill)} />
+          <p className="my-0 mx-2">{skill}</p>
+        </div>
+      ))}
+      <div className="flex items-center bg-gray-300 mx-4 p-2">
+        <CloseButton />
+        <form onSubmit={add_skill}>
+          <input type="text" name="skill" placeholder="Enter in a new skill"/>
+          <input type="submit" value="Enter" />
+        </form>
+      </div>
+    </div>
+  );
+  const work = (
+    <p className="mx-4">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint dolorum iure
+      blanditiis officia vero ipsa magnam ipsum sit quidem veritatis, quam
+      quaerat laboriosam corporis harum recusandae odio error reprehenderit
+      autem?
+    </p>
+  );
   return (
     <div className="w-11/12 m-auto">
       <div className="float-left w-1/4 p-4">
@@ -55,7 +106,9 @@ const ProfilePage = () => {
 
         <div className="relative rounded-3xl mb-16 grid justify-items-center shadow-xl">
           <div className="flex relative rounded-t-3xl bg-green-600 w-full h-20 justify-center items-center">
-            <p className="text-white font-semibold text-2xl">Elevator Pitch Video</p>
+            <p className="text-white font-semibold text-2xl">
+              Elevator Pitch Video
+            </p>
           </div>
           <ModalVideo
             channel="youtube"
@@ -78,35 +131,9 @@ const ProfilePage = () => {
         </div>
       </div>
       <div className="float-left w-3/4 p-4">
-        <div className="relative rounded-3xl m-4 grid justify-items-center">
-          <div className="rounded-t-3xl bg-green-600 w-full h-20"></div>
-          <p className="z-100">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
-            dolorum iure blanditiis officia vero ipsa magnam ipsum sit quidem
-            veritatis, quam quaerat laboriosam corporis harum recusandae odio
-            error reprehenderit autem?
-          </p>
-        </div>
-
-        <div className="relative rounded-3xl m-4 grid justify-items-center">
-          <div className="rounded-t-3xl bg-green-600 w-full h-20"></div>
-          <p className="z-100">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
-            dolorum iure blanditiis officia vero ipsa magnam ipsum sit quidem
-            veritatis, quam quaerat laboriosam corporis harum recusandae odio
-            error reprehenderit autem?
-          </p>
-        </div>
-
-        <div className="relative rounded-3xl m-4 grid justify-items-center">
-          <div className="rounded-t-3xl bg-green-600 w-full h-20"></div>
-          <p className="z-100">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
-            dolorum iure blanditiis officia vero ipsa magnam ipsum sit quidem
-            veritatis, quam quaerat laboriosam corporis harum recusandae odio
-            error reprehenderit autem?
-          </p>
-        </div>
+        <Section name="Summary" content={summary} />
+        <Section name="Skills" content={displayskills} />
+        <Section name="Work Experiences" content={work} />
       </div>
     </div>
   );
---
+++ codebase/frontend/src/profile-page/section.tsx
@@ -0,0 +1,15 @@
+import "tailwindcss/tailwind.css";
+
+const Section = (props) => {
+
+  return (
+    <div className="relative rounded-3xl mb-16 grid shadow-xl pb-8">
+      <div className="flex relative rounded-t-3xl bg-green-600 w-full h-20 items-center mb-4">
+        <p className="text-white font-semibold text-2xl my-0 ml-4">{props.name}</p>
+      </div>
+      {props.content}
+    </div>
+  );
+};
+
+export default Section;
