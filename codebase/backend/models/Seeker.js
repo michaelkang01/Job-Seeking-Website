4419a731e3436e57f6aae945c16c6db12208dcf5
Seyon Kuganesan <seyonkuganesan@gmail.com>
2021-10-04 20:10:18 -0400

    added ref between user and job seeker objects

--- codebase/backend/models/Seeker.js
+++ codebase/backend/models/Seeker.js
@@ -2,6 +2,7 @@ import crypto from 'crypto';
 
 export default mongoose => {
     const seekerSchema = new Schema({
+        userID: Schema.Types.ObjectId,
         email: String,
         firstName: String,
         lastName: String,   
@@ -10,8 +11,8 @@ export default mongoose => {
         facebookID: String,
         resumeUrl: String,
         summary: String,
-        workExperience: { type: Array}, // {exp1: {title, start, end, location, description}, exp2: [...]}
-        education: { type: Array}, // {ed1: [schoolName, start, end, location}}
+        workExperience: { type: Array }, // {exp1: {title, start, end, location, description}, exp2: [...]}
+        education: { type: Array }, // {ed1: [schoolName, start, end, location}}
         skills: [String], // "skills" : ["Express", "Ruby"]
     });
     return mongoose.model("Seeker", seekerSchema);
--- codebase/backend/models/User.js
+++ codebase/backend/models/User.js
@@ -1,9 +1,16 @@
 
 const bcrypt = require('bcrypt');
 const crypto = require('crypto');
+const { Schema } = require('mongoose');
+
+// mongo db refs
+// original_id = ObjectId()
+// db.users.insert({"_id": original_id, "email": ...})
+// db.jobseekerprofiles.insert({"userID": original_id, ...})
 
 module.exports = mongoose => {
 	const UserSchema = new mongoose.Schema({
+		_id: Schema.Types.ObjectId,
 		email: String,
 		password: String,
 		firstName: String,
