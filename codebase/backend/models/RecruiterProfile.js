dfe394c867ffabf440c4c0663466c946f7a365f2
Seyon Kuganesan <seyonkuganesan@gmail.com>
2021-10-16 20:34:58 -0400

    created recruiter model

---
+++ codebase/backend/models/RecruiterProfile.js
@@ -0,0 +1,11 @@
+module.exports = mongoose => {
+	const RecruiterProfileSchema = new mongoose.Schema({
+			email: String,
+			firstName: String,
+			lastName: String,
+            companyName: String,
+			jobsPosted: [{ type: Schema.Types.ObjectId }],
+			metadata: Array,
+		}, { timestamps: true });
+	return mongoose.model("RecruiterProfile", RecruiterProfileSchema);
+};
