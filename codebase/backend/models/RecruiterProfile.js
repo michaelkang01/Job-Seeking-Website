f69261ef8e56a9f102f6bc7e211b8dab6d1fcbf0
Seyon Kuganesan <seyonkuganesan@gmail.com>
2021-10-16 22:49:55 -0400

    fixed code formatting

--- codebase/backend/models/RecruiterProfile.js
+++ codebase/backend/models/RecruiterProfile.js
@@ -1,11 +1,10 @@
 module.exports = mongoose => {
 	const RecruiterProfileSchema = new mongoose.Schema({
-			email: String,
-			firstName: String,
-			lastName: String,
-            companyName: String,
-			jobsPosted: [{ type: Schema.Types.ObjectId }],
-			metadata: Array,
-		}, { timestamps: true });
+		email: String,firstName: String,
+		lastName: String,
+		companyName: String,
+		jobsPosted: [{ type: Schema.Types.ObjectId }],
+		metadata: Array,
+	}, { timestamps: true });
 	return mongoose.model("RecruiterProfile", RecruiterProfileSchema);
 };
