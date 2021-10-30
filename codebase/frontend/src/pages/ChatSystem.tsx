6afe5eb782fdcbc80e7db65cbbb7d0537331eb7f
FlamesRunner <akyle2001@gmail.com>
2021-10-30 02:10:41 -0400

    Minor styllistic fixes

--- codebase/frontend/src/pages/ChatSystem.tsx
+++ codebase/frontend/src/pages/ChatSystem.tsx
@@ -1,5 +1,7 @@
+import { randomBytes } from "crypto";
 import React from "react";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
 type UserTableRow = {
@@ -34,7 +36,8 @@ const ChatSystem = () => {
     const [chatSessions, setChatSessions] = useState<Array<ChatInstance>>([]);
     const [userTable, setUserTable] = useState<UserTable>({});
     const [selectedChat, setSelectedChat] = useState<ChatInstance | undefined>();
-    const chime = new Audio('../assets/message_received.mp3');
+    const chime = new Audio('/assets/message_received.mp3');
+    const [disconnected, setDisconnected] = useState(false);
 
     // Attempt to establish a websocket connection to the server
     useEffect(() => {
@@ -102,21 +105,30 @@ const ChatSystem = () => {
                 setLoading(false);
             }
 
+            newWebSocket.onclose = () => {
+                setDisconnected(true);
+            }
+
             setWebSocket(newWebSocket);
         }
     }, [loading, webSocket, authToken]);
 
     // Show chat interface, and loading screen if websocket connection is not established
-    if (loading || !authData || !webSocket || webSocket.readyState !== webSocket.OPEN) {
+    if ((loading || !authData || !webSocket || webSocket.readyState !== webSocket.OPEN) && !disconnected) {
         return <div className="pt-32 text-2xl text-center"><h1>Messaging system loading...</h1></div>;
+    }
+
+    if (disconnected) {
+        return <div className="pt-32 text-2xl text-center"><h1>You've been disconnected. Click <a className="font-bold underline" href={`/messages/?${randomBytes(6).toString('hex')}`}>here</a> to attempt reconnection.</h1></div>;
     }
 
     // Messages interface (show sidebar with messages, tailwindcss)
     return (
         <div className="flex h-screen gap-1 bg-blue-400">
-            <div className="col-span-3 bg-gray-100 md:w-1/6 flex-row" style={{
+            <div className="col-span-3 bg-gray-100 md:w-1/6 flex-row overflow-y-scroll" style={{
                 paddingTop: "88px"
             }}>
+                <h3 className="text-2xl text-center py-4 text-gray-900 font-bold" style={{ backgroundColor: "#9AC2C9" }}>Conversations</h3>
                 {chatSessions.map((chatSession) => {
                     const talkingTo = chatSession.personOne === JSON.parse(authData).payload.id ? chatSession.personTwo : chatSession.personOne;
                     const talkingToUser = (userTable && userTable[talkingTo]) || { firstName: talkingTo, lastName: "" };
@@ -125,7 +137,7 @@ const ChatSystem = () => {
                             setSelectedChat(chatSession);
                         }} className={`w-full px-4 py-4 ${chatSession === selectedChat ? "bg-yellow-100" : "bg-gray-200"}`} key={JSON.parse(authData).payload.id + talkingTo}>
                             <p className="font-bold">{talkingToUser.firstName + " " + talkingToUser.lastName}</p>
-                            <p className="text-sm">{chatSession.messages[chatSession.messages.length - 1].message.substr(0, 50)}...</p>
+                            <p className="text-sm">Last message: {chatSession.messages[chatSession.messages.length - 1].message.substr(0, 36)}...</p>
                         </div>
                     )
                 })}
@@ -134,12 +146,12 @@ const ChatSystem = () => {
                 paddingTop: "88px"
             }}>
                 {selectedChat && (
-                    <div className="flex-row h-full">
-                        <div className="border-t-2 border-black" style={{  }}>
+                    <div className="flex-row h-full bg-blue-300">
+                        <div className="border-t-4 border-blue-300" style={{  }}>
                             <form className="" onSubmit={(event) => {
                                 event.preventDefault();
                                 const message = (event.target as any).elements.message.value;
-                                if (message && message.length > 0) {
+                                if (message && message.length > 0 && webSocket) {
                                     webSocket.send(JSON.stringify({
                                         type: "send",
                                         token: authToken,
@@ -149,22 +161,26 @@ const ChatSystem = () => {
                                     (event.target as any).elements.message.value = "";
                                 }
                             }}>
-                                <input className="w-full px-4 py-2 bg-gray-200 border-b-2 border-black" type="text" name="message" placeholder="Type a message..." />
+                                <input className="w-full px-4 py-2 bg-gray-200 mb-1" type="text" name="message" placeholder="Type a message..." />
                             </form>
                         </div>
-                        <div className="flex-grow overflow-y-scroll" style={{ height: "89vh" }}>
+                        <div className="flex-grow overflow-y-scroll" style={{ height: "calc(100% - 53px)" }}>
                             <div className="flex-col">
                                 {selectedChat.messages.slice().reverse().map((messageObject) => {
                                     // From
                                     const from = (userTable && userTable[messageObject.sender]) || { firstName: messageObject.sender, lastName: "" };
-
+                                    // If message is from current user, show message in green
+                                    const fromClass = messageObject.sender === JSON.parse(authData).payload.id ? "bg-green-100" : "bg-gray-100";
                                     return (
-                                        <div className="w-full px-4 py-4 bg-gray-200 border-b-2 border-black" key={messageObject.id}>
+                                        <div className={`w-full px-4 py-4 ${fromClass} border-b-2 border-black`} key={messageObject.id}>
                                             <p className="font-bold">{from.firstName + " " + from.lastName} ({(new Date(messageObject.when)).toLocaleString()})</p>
                                             <p className="text-sm">{messageObject.message}</p>
                                         </div>
                                     )
                                 })}
+                                <p className="w-full text-center italic py-4">
+                                    End of conversation
+                                </p>
                             </div>
                         </div>
                     </div>
