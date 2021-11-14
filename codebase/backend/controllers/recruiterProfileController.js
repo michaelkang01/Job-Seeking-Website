2372b2e9bafa0540f2028421ed019a690db96c91
dz365 <65931281+dz365@users.noreply.github.com>
2021-11-14 16:39:51 -0500

    TEAM418-90 Comment changes

--- codebase/backend/controllers/recruiterProfileController.js
+++ codebase/backend/controllers/recruiterProfileController.js
@@ -3,7 +3,7 @@ const { verifyUser } = require("../middleware/auth");
 const RecruiterProfileRoute = (router, RecruiterProfile) => {
   
   /**
-   * @api {post} /api/recruiter/create Create a jobseeker profile
+   * @api {post} /api/recruiter/create Create a recruiter profile
    */
    router.post(`/recruiter/create`, (req, res) => {
     RecruiterProfile.create({
